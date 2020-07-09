fd50c076ad39626ae932108747944d66ed0c9d8f
Roland Kammerer <roland.kammerer@linbit.com>
2020-07-09 16:54:08 +0200

    AddEnv: add some shell quoting

--- env.go
+++ env.go
@@ -3,18 +3,23 @@ package sshclient
 import (
 	"fmt"
 	"strings"
+
+	"github.com/alessio/shellescape"
 )
 
 // AddEnv takes a script and prepends it with export lines from the env variable slice.
 // Entries in env should have a VAR=val syntax
-// Note that this currently does not do any checks!
+// Note that this currently does only a limited number of checks
 // There is a ssh.session.Setenv(), but usually ssh, for good reasons, does only allow to set a
 // limited set of variables/no user variables at all.
 func AddEnv(script string, env []string) (string, error) {
 	var envBlob string
 	for _, kv := range env {
 		kvs := strings.SplitN(kv, "=", 2)
-		envBlob += fmt.Sprintln(kv, "; export ", kvs[0])
+		if kvs[0] != shellescape.Quote(kvs[0]) {
+			return "", fmt.Errorf("shell variable name not valid")
+		}
+		envBlob += fmt.Sprintf("%s=%s; export %s\n", kvs[0], shellescape.Quote(kvs[1]), kvs[0])
 	}
 
 	return fmt.Sprint(envBlob, script), nil
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/LINBIT/gosshclient
 
 go 1.13
 
-require golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+require (
+	github.com/alessio/shellescape v1.2.2
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+)
